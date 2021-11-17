7eed459727a3b3dd477100256bc61cf5e28bce13
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-11-17 11:26:53 -0300

    Aprendo mais selects e adicionando atores e diretores nas tabelas

--- 2-SELECT.sql
+++ 2-SELECT.sql
@@ -1,5 +1,8 @@
 #SELECT 
 
+show databases;
+
+use dbfilmesturmab;
 /*
 Select - especifica as colunas que serão visualizadas 
 from - especifica as tabelas que seraão utilizadas na consulta
@@ -39,21 +42,129 @@ SELECT nome, sinopse FROM tblFilme WHERE idFilme = 2;
     Menor que - <
     Maior ou igual que - >=
     Menor ou igual que - <=
-    Like - Semelhante ao um igual, porem com menos igualdade
+    Like - Semelhante ao um igual, porem com menos igualdade (busca pela igualdade do valor)
  */
 
+/*OPERADORES LÓGICOS
+	and
+    or 
+    not
+*/
 #quando fazemos uma comparação entre strings com igual, ela deve ser igual ao valor do campo para que o banco ache
 SELECT nome, sinopse FROM tblFilme WHERE nome = 'Um sonho de liberdade';
 #Porcentagem no inicio - A string dada deve ser igual ao termino do valor do campo
 SELECT nome FROM tblFilme WHERE nome LIKE '%liberdade';
 #Porcentagem no fim -  A string dada deve ser igual ao inicio do valor do campo
-SELECT nome FROM tblFilme WHERE nome LIKE '%Um';
+SELECT nome FROM tblFilme WHERE nome LIKE 'Um%';
 #Porcentagem em ambos - A string dada deve ser igual a qualquer palavra do valor do campo
 SELECT nome FROM tblFilme WHERE nome LIKE '%sonho%';
 
 #Maisculo  minusculo não importam, pesquisa da mesma forma
 #As porcentagens são do workbanch/mysql, ou seja, pode mudar o simbolo dependendo do banco
 
+
+alter table tblFilme 
+	add column qtde int;
+    
+update tblFilme set qtde = 5 where idFilme = 1;
+update tblFilme set qtde = 8 where idFilme = 2;
+
+select * from tblFilme where qtde <=6;
+
+#Exemplo de utilização de operadores lógicos
+select * from tblFilme where qtde >=6 and qtde <=10;
+select * from tblFilme where qtde <6 or qtde >10;
+select * from tblFilme where not (qtde >=6 and qtde <=10);
+
+INSERT INTO tblFilme (nome,
+nomeOriginal,
+duracao,
+dataLancamento,
+dataRelancamento,
+sinopse,
+fotoCapa,
+idClassificacao,
+qtde) 
+#Duracao - hora:min:sec
+#Data - 4ano-2mes-2dia 
+#Quando temos um atributo que não recebe valor, devemos colocar null
+VALUES ('FORREST GUMP - O CONTADOR DE HISTÓRIAS',
+'Forrest Gump',
+ '02:20:00', 
+ '1994-12-07', 
+ null, 
+ 'Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump 
+ (Tom Hanks), um rapaz com QI abaixo da média e boas intenções. Por obra do acaso, 
+ ele consegue participar de momentos cruciais, como a Guerra do Vietnã e Watergate, 
+ mas continua pensando no seu amor de infância, Jenny Curran.', 
+'filme03.jpg', 
+4,
+3),(
+'À ESPERA DE UM MILAGRE',
+'The Green Mile',
+ '03:09:00', 
+ '2000-03-10', 
+ null, 
+ '1935, no corredor da morte de uma prisão sulista. Paul Edgecomb (Tom Hanks) 
+ é o chefe de guarda da prisão, que temJohn Coffey (Michael Clarke Duncan) 
+ como um de seus prisioneiros. Aos poucos, desenvolve-se entre eles uma relação 
+ incomum, baseada na descoberta de que o prisioneiro possui um dom mágico que 
+ é, ao mesmo tempo, misterioso e milagroso.', 
+'filme04.jpg', 
+4,
+4);
+
+#select repetitiva
+select * from tblFilme where qtde =2 or qtde =5 or qtde =8 or qtde =10;
+#o comando IN substitui uma escrita repetitiva
+#apenas utilizado quando for OR, não funciona com AND
+select * from tblFilme where qtde in (2, 5, 8, 10);
+#in negativo
+select * from tblFilme where qtde not in (2, 5, 8, 10);
+
+#seleciona numeros entre...
+select * from tblFilme where qtde between 6 and 10;
+select * from tblFilme where qtde not between 6 and 10;
+
+
+#ORDENAÇÃO DE DADOS
+#asc - Ascendente (crescente)
+select * from tblFilme order by nome asc;
+#desc - descendente (decrescente)
+select * from tblFilme order by nome desc;
+
+
+select * from tblFilme order by nome asc, duracao desc;
+
+#limit - limita a quantidade de resultados
+select * from tblFilme limit 1;
+
+#somente o 1 registro da tabela
+select * from tblFilme order by idFilme asc limit 1;
+
+#somente o ultimo registro da tabela
+select * from tblFilme order by idFilme desc limit 1;
+
+
+#count - retorna a quantidade de itens no select
+#com o apelido conseguimos resgatar
+#ignora valores nulos
+#não pode haver espaços entre as funções do select, pois vai achar que estamos apelidamos uma função
+select count(*) as qtdeDeFilmes from tblFilme;
+select count(nome) as qtdeNomeFilmes, count(dataRelancamento) from tblFilme;
+
+#min - quando queremos saber qual a quantidade minima de certo campo, retornando o menor valor 
+select min(qtde) as qtdeMinima from tblFilme;
+#max - quando queremos saber qual a quantidade maxima de certo campo, retornando o maior valor 
+select max(qtde) as qtdeMaxima from tblFilme;
+
+#soma todos os campos de uma coluna
+select sum(qtde) as somaQtdeFilmes from tblFilme;
+
+#média de todos os campos de uma coluna
+select avg(qtde) as somaQtdeFilmes from tblFilme;
+#limitamos o resultado em uma casa pos a virgula
+select round(avg(qtde), 1)  as somaQtdeFilmes from tblFilme;
 
 
 
--- ScriptFilme.sql
+++ ScriptFilme.sql
@@ -352,3 +352,117 @@ DELETE  FROM tblGeneroFilme WHERE idGeneroFilme = 10;
 DELETE  FROM tblGeneroFilme WHERE idGeneroFilme = 1; 
 #os ids daqueles que foram apagados nunca mais voltam
 
+
+show tables;
+
+#TABELA GENERO PESSOA
+select * from tblGeneroPessoa;
+insert into tblGeneroPessoa(nome) values ("Feminino");
+insert into tblGeneroPessoa(nome) values ("Masculino");
+insert into tblGeneroPessoa(nome) values ("N binário");
+insert into tblGeneroPessoa(nome) values ("Binário");
+
+
+#TABELA ATOR
+	#REI LEÃO
+select * from tblAtor;
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Garcia Júnior", null, "1967-03-02", null, null, 2, "fotoAtor1");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Jorgeh Ramos", null,  "1941-02-03", "2014-12-01", "Jorgeh Ramos foi ator, diretor de dublagem, 
+autor de peças teatrais, já apresentou alguns telejornais, e foi um dos diretores 
+artíticos da produtora Herbert Richards, hoje extinta. Entretanto, Jorgeh ficou mais 
+conhecido por ser pioneiro na profissão dublador de filmes e narrador de trailers de cinema. 
+Ele realizou a dublagem dos vilões Jafar, Scar e Rasputin, dos desenhos Aladdin, O Rei Leão 
+e Anastasia, respectivamente, e de Peter McCallister, o pai do garotinho de Esqueceram de Mim, 
+entre outros." , 2, "fotoAtor2");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Mauro Ramos", null,  "1961-02-13", null, null , 2, "fotoAtor2");
+
+	#UM SONHO DE LIBERDADE
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Timothy Francis Robbins","Tim Robbins",  "1958-10-16", null, null, 2, "fotoAtor4");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Morgan Freeman", null,  "1937-06-01", null, "Possui uma estrela na Calçada da Fama, localizada em 7021 Hollywood Boulevard.", 2, "fotoAtor5");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Bob Gunton", null,  "1945-11-15", null, null, 2, "fotoAtor6");
+
+	#A ESPERA DE UM MILAGRE
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Thomas Jeffrey Hanks", "Tom Hanks",  "1956-06-09", null, 'Tom Hanks iniciou a carreira no cinema aos 24 anos, com um papel no filme de baixo orçamento Trilha de Corpos. Logo migrou para a TV, onde estrelou por dois anos a série Bosom Buddies. Nela passou a trabalhar com comédia, algo com o qual não estava habituado, rendendo convites para pequenas participações nas séries Táxi, Caras & Caretas e Happy Days.
+
+Em 1984, veio seu primeiro sucesso no cinema: a comédia Splash - Uma Sereia em Minha Vida, na qual era dirigido por Ron Howard e contracenava com Daryl Hannah. Em seguida vieram várias comédias, entre elas A Última Festa de Solteiro (1984), Um Dia a Casa Cai (1986) e Dragnet - Desafiando o Perigo (1987), tornando-o conhecido do grande público.
+
+Com Quero Ser Grande (1988) Hanks obteve sua primeira indicação ao Oscar de melhor ator. Apesar de consagrado como comediante, aos poucos passou a experimentar outros gêneros. Em 1993 surpreendeu em Filadélfia como um advogado homossexual que luta na justiça contra sua demissão, motivada por preconceito devido a ele ser portador do vírus da AIDS. Pelo papel conquistou seu primeiro Oscar.
+
+Já no ano seguinte Hanks ganharia sua segunda estatueta dourada, repetindo um feito apenas obtido por Spencer Tracy, quase 60 anos antes. Forrest Gump - O Contador de Histórias foi sucesso de público e crítica, ganhando seis Oscar.
+
+Estabelecido como um dos maiores astros de Hollywood, Hanks passou a emendar um sucesso atrás do outro: Apollo 13 - Do Desastre ao Triunfo (1995), Toy Story (1995), O Resgate do Soldado Ryan (1998), Mens@gem Para Você (1998), Toy Story 2 (1999), À Espera de um Milagre (1999), Náufrago (2000), Prenda-me Se For Capaz (2002) e Estrada para Perdição (2002).
+
+Em 1996, resolveu se dedicar à sua estreia na direção. O resultado foi The Wonders - O Sonho Não Acabou, divertida comédia que rendeu a contagiante música "That Thing You Do!". Hanks voltaria a trabalhar como diretor em episódios das séries de TV Da Terra à Lua (1998) e Band of Brothers (2001) e também na comédia romântica Larry Crowne - O Amor Está de Volta (2011).
+
+Em 2004, o ator encampou a ideia de interpretar vários personagens na animação O Expresso Polar, dirigida pelo colega Robert Zemeckis. Usando o método de captura de movimento, Hanks interpretou seis personagens de idades variadas.
+
+Um de seus personagens mais famosos é o simbologista Robert Langdon, criado pelo autor Dan Brown. Hanks o interpretou em dois filmes, O Código Da Vinci (2006) e Anjos e Demônios (2009).', 2, "fotoAtor7");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Michael Clarke Duncan", "Michael Duncan",  "1957-12-10", null, "Michael Clarke Duncan é conhecido pela atuação em À Espera de um Milagre, que lhe rendeu indicações ao Oscar e ao Globo de Ouro de Melhor Ator Coadjuvante. Fez sua estreia nos cinemas em 1995, com um papel não creditado em Sexta-Feira em Apuros. O primeiro trabalho de destaque viria três anos depois com Armageddon. Agradou tanto que Bruce Willis recomendou que Frank Darabont contratasse ele para À Espera de um Milagre, em 1999.
+
+Muitas vezes tratado como Big Mike, por causa da altura de 1,96 m, o ator se destacou ainda em Meu Vizinho Mafioso, Planeta dos Macacos, O Escorpião Rei e A Ilha. Participou também de três adaptações dos quadrinhos: O Demolidor, Sin City - A Cidade do Pecado e Lanterna Verde. Robert Rodriguez contava com o retorno dele para Sin City 2: A Dame to Kill For, algo que infelizmente não irá mais acontecer.
+
+Na TV, Clarke Duncan contou com participações em importantes seriados, como Um Maluco no Pedaço, Bones, Chuck e Two and a Half Men. Faleceu em setembro de 2012, aos 54 anos, após passar dois meses hospitalizado em Los Angeles.", 2, "fotoAtor8");
+    
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("David Bowditch Morse", "David Morse",  "1953-10-11", null, null, 2, "fotoAtor11");    
+    
+    #FORREST GUMP
+    #TOM HANKS
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Gary Alan Sinise", "Gary Sinise",  "1955-03-17", null, null, 2, "fotoAtor9");
+
+insert into tblAtor(nome, nomeArtistico, dataNascimento, dataFalecimento, biografia, idGeneroPessoa, foto)
+values ("Robin Wright Penn", "Robin Wright",  "1966-04-08", null, "Robin Wright nasceu em Dallas, Texas, filha de Gayle Gaston, uma vendedora de cosméticos, e Freddie Wright, funcionário de uma empresa farmacêutica. A atriz ficou famosa por seus papeis em House of Cards, Mulher-Maravilha, Corpo Fechado e Forrest Gump - O Contador de Histórias.
+
+Wright foi criada em San Diego, Califórnia. Quando jovem, frequentou a La Jolla High School, em La Jolla, e a Taft High School em Woodland Hills, em Los Angeles.
+
+A atriz começou sua carreira como modelo quando tinha 14 anos. Aos 18, interpretou Kelly Capwell na novela Santa Barbara (1984), da NBC Daytime, recebendo diversas indicações ao Daytime Emmy e voltando os olhos do público para seu trabalho.
+
+Das telas da TV para o cinema, Wright conseguiu um papel em Hollywood Vice Squad (1986) e protagonizou uma princesa em A Princesa Prometida, em 1987. Alguns anos mais tarde, a atriz foi aclamada pela crítica em seu papel como Jenny Curran, em Forrest Gump (1994), recebendo indicações ao Golden Globe Award e Screen Actors Guild de Melhor Atriz Coadjuvante.
+
+Em 1996, ela protagonizou a adaptação cinematográfica de Moll Flanders, do diretor Daniel Defoe, recebendo uma indicação ao Satellite Award de Melhor Atriz em Drama. No ano seguinte, Wright foi indicada ao Screen Actors Guild Award de Melhor Atriz por seu papel em Loucos de Amor, filme no qual ela co-estrelou com seu então marido, Sean Penn.
+
+Contracenando com Bruce Willis e Samuel L. Jackson, a atriz atuou no suspense Corpo Fechado, dirigido por M. Night Shyamalan, em 2000.
+
+Em 2013, Wright apareceu na série da Netflix, House of Cards, interpretando a implacável Claire Underwood, esposa do político Frank Underwood (Kevin Spacey). Em 2014, ela ganhou um Globo de Ouro pelo papel, tornando-se a primeira atriz a ganhar o prêmio por uma série de televisão com transmissão apenas online. A atriz foi indicada para o mesmo prêmio no ano seguinte e também recebeu indicações para o Primetime Emmy Award em 2013 e 2014, pelo mesmo papel. Após a saída de Spacey por conta das acusações de má conduta sexual, Wright foi definida como a nova protagonista do show, continuando a ser indicada a diversos prêmios.
+
+Em 2017, a atriz interpretou a General Antíope em Mulher-Maravilha, ao lado de Gal Gadot e Chris Pine. No mesmo ano, ela apareceu na superprodução sequência de Blade Runner, Blade Runner 2049, ao lado de Ryan Gosling, Harrison Ford e Jared Leto. O longa foi altamente aclamado pela crítica.
+
+Wright é porta-voz honorária da Fundação Gordie, sem fins lucrativos, com sede no Texas. Em 2014, a atriz fez parceria com duas empresas sediadas na Califórnia; Pour Les Femmes e The SunnyLion. Essa última doa uma parte de seus lucros ao movimento Raise Hope For Congo.
+
+A atriz é ativista dos direitos humanos na República Democrática do Congo. Ela é a narradora e produtora executiva do documentário When Elephants Fight, que destaca como corporações multinacionais de mineração e políticos ameaçam os direitos humanos e perpetuam conflitos na região.", 1, "fotoAtor10");
+    
+
+#TABELA DIRETOR
+select * from tblDiretor;
+	#REI LEÃO
+    insert into tblDiretor(nome, dataNascimento, nomeArtistico, biografia, foto, idGeneroPessoa)
+values ("Roger Charles Allers", "1949-06-29", 'Roger Allers', null, 'fotoDiretor1', 2);
+
+insert into tblDiretor(nome, dataNascimento, nomeArtistico, biografia, foto, idGeneroPessoa)
+values ("Rob Ralph Minkoff", "1962-08-11", 'Rob Minkoff', null, 'fotoDiretor2', 2);
+
+    #UM SONHO DE LIBERDADE
+    
+    
+	#A ESPERA DE UM MILAGRE
+insert into tblDiretor(nome, dataNascimento, nomeArtistico, biografia, foto, idGeneroPessoa)
+values ("Ferenc Árpád Darabont", "1959-01-28", 'Frank Darabont', ' É o autor dos roteiros de 
+"A Hora do Pesadelo 3", "A Mosca 2" e "Frankenstein de Mary Shelley".- Trabalhou como 
+roteirista na série de TV norte-americana "O Jovem Indiana Jones".', 'fotoDiretor1', 2);
+	#FOREST GUMP
+
